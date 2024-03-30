cc91d9a658dc9094991612669332c2be7c3a6f60
Zohaib <zohaib.anwar0310@gmail.com>
2024-03-31 00:56:17 +0500

    Worked with nested routes

---
+++ src/app/blogs/first/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FirstBlog = () => {
+  return <h1>My First Blog Page</h1>;
+};
+
+export default FirstBlog;
---
+++ src/app/blogs/second/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SecondBlog = () => {
+  return <h1>My Second Blog Page</h1>;
+};
+
+export default SecondBlog;
